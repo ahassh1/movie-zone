3217c307e1ab884f6c19f27eefa5a7d3a1c32a94
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-08-12 22:34:12 +0600

    react router project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold underline text-3xl">hello world </h1>
+    </>
+  );
+}
+
+export default App;
