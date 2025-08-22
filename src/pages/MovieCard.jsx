15a55ab40efa72c793a7fd9b602e943dda803db4
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-08-23 01:09:21 +0600

    slice added

--- src/pages/MovieCard.jsx
+++ src/pages/MovieCard.jsx
@@ -4,7 +4,7 @@ const MovieCard = ({ movie }) => {
   const { title, image, description, cast, director } = movie;
   return (
     <div>
-      <div className="card bg-sky-100 w-96 shadow-xl/20">
+      <div className="card bg-sky-100 w-96 shadow-xl/20 my-4">
         <figure>
           <img
             className=" w-[320px] h-[330px] object-cover"
@@ -13,13 +13,30 @@ const MovieCard = ({ movie }) => {
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{title}</h2>
-          <p>{description}</p>
+          <div className="justify-between items-center flex">
+            <div>
+              <h2 className="card-title">{title}</h2>
+            </div>
+            <div>
+              <h2>
+                <span className="font-semibold">Cast:</span> {cast.slice(0, 20)}
+              </h2>
+            </div>
+          </div>
+          <p className="text-gray-600">{description.slice(0, 100)}</p>
 
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary hover:bg-orange-600 transition duration-500 hover:border-none border-none">
-              More Details
-            </button>
+          <div className="card-actions justify-between items-center">
+            <div>
+              <h3 className="font-semibold text-[15px]">
+                Director: <span className="text-gray-500">{director}</span>
+              </h3>
+            </div>
+            <div>
+              {" "}
+              <button className="btn btn-primary hover:bg-orange-600 transition duration-500 hover:border-none border-none">
+                More Details
+              </button>
+            </div>
           </div>
         </div>
       </div>
