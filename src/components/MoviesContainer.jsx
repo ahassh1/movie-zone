58b80412b846f94387c9e42d8970046f241a54d0
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-08-24 13:05:44 +0600

    showall and showless done

--- src/components/MoviesContainer.jsx
+++ src/components/MoviesContainer.jsx
@@ -21,7 +21,10 @@ const MoviesContainer = ({ movies }) => {
         ))}
       </div>
       <button
-        onClick={() => setShowAll((prv) => !prv)}
+        onClick={() => {
+          setShowAll((prv) => !prv);
+          if (showAll) window.scroll(0, 600);
+        }}
         href="#_"
         className="px-5 py-2.5 relative rounded group font-medium text-white mt-2 md:mt-5 lg:mt-7 xl:mt-9 inline-block cursor-pointer"
       >
@@ -29,7 +32,7 @@ const MoviesContainer = ({ movies }) => {
         <span className="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
         <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
         <span className="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
-        <span className="relative">Show All</span>
+        <span className="relative">{showAll ? "Show less" : "Show All"}</span>
       </button>
     </div>
   );
