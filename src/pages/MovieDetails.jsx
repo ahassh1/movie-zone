3f385253b7dcd288d1f0bef4e233169e7d6e58b9
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-09-01 11:25:19 +0600

    find use

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -4,8 +4,10 @@ import { useLoaderData, useParams } from "react-router";
 const MovieDetails = () => {
   const data = useLoaderData();
   const { id } = useParams();
-  console.log(id);
-  console.log(data);
+  // console.log(id);
+  // console.log(data);
+  const singlePhone = data.find((phone) => phone.id === parseInt(id));
+  console.log(singlePhone);
   return (
     <div>
       <h2>Movie Details</h2>
